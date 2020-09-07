41e8caa6f4b5bc566e8807629dedab7bc06d3b65
liurj1 <liurj@yunjiglobal.com>
2020-09-07 16:19:29 +0800

    按需加载

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: liuruijun
  * @Date: 2020-06-01 17:02:20
  * @LastEditors: liuruijun
- * @LastEditTime: 2020-09-07 14:40:40
+ * @LastEditTime: 2020-09-07 16:19:14
  * @Description: file content
  */
 
@@ -14,6 +14,9 @@ import store from './redux/store'
 <% } %>
 import App from './containers/App/App'
 import './assets/less/reset.less'
+<% if (!isH5 && uiLibrary === 'ant-design') { %>
+import 'antd/dist/antd.less'
+<% } %>
 
 ReactDOM.render(
     <% if (needRedux) { %>
