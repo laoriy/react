9316d5fb2ac91d7355ab3b8dcac54b602c7d8ba6
liurj1 <liurj@yunjiglobal.com>
2020-09-07 14:40:56 +0800

    ss

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,24 @@
  * @Author: liuruijun
  * @Date: 2020-09-04 14:13:05
  * @LastEditors: liuruijun
- * @LastEditTime: 2020-09-04 15:24:46
+ * @LastEditTime: 2020-09-07 14:39:12
  * @Description: file content
  */
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'airbnb'
   ],
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   plugins: [
-    'react',
+    'react'
   ],
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: ['error', 'never'], // 禁用 分号
@@ -35,13 +35,13 @@ module.exports = {
     'no-shadow': 0, // 禁用  禁止页面内相容参数名
     'object-shorthand': 0, // 禁用 禁止对象内使用带引号字符串
     'no-restricted-syntax': 0,
-    "no-plusplus": 0, // 禁用 ++
+    'no-plusplus': 0, // 禁用 ++
     'consistent-return': 0, // 关闭箭头函数必须要return
     'no-return-assign': 0, // return 语句中不能有赋值表达式
-    'global-require': 0,  // 关闭禁止使用requrie
-    'prefer-promise-reject-errors': 0, //这条规则旨在确保承诺只被Error对象拒绝。
-    "import/extensions": "off", // 禁用文件名详细文件类型后缀
-    "react/jsx-props-no-spreading":0,
-    "react/prefer-stateless-function":0
-  },
-};
+    'global-require': 0, // 关闭禁止使用requrie
+    'prefer-promise-reject-errors': 0, // 这条规则旨在确保承诺只被Error对象拒绝。
+    'import/extensions': 'off', // 禁用文件名详细文件类型后缀
+    'react/jsx-props-no-spreading': 0,
+    'react/prefer-stateless-function': 0
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@
  * @Author: liuruijun
  * @Date: 2020-06-01 17:02:20
  * @LastEditors: liuruijun
- * @LastEditTime: 2020-09-07 09:50:11
+ * @LastEditTime: 2020-09-07 14:40:40
  * @Description: file content
  */
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './containers/App/App'
-import './assets/less/reset.less'
 <% if (needRedux) { %>
 import { Provider } from 'react-redux'
 import store from './redux/store'
 <% } %>
-
+import App from './containers/App/App'
+import './assets/less/reset.less'
 
 ReactDOM.render(
     <% if (needRedux) { %>
